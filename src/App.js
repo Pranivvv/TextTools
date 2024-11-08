556e8cf66be8a8ae3051faa22ce82a007a6e50d9
Pranivvv <21104031.praniv.warungshe@gmail.com>
2024-11-08 16:30:14 +0530

    removed router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import About from './components/About';
+// import About from './components/About';
 import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextBox from './components/TextBox';
 import React, { useState } from 'react';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
  
 
 function App() {
   const [mode, setMode] = useState('light')
   const [alert, setAlert] = useState(null)
-  const [myStyle, setMyStyle] = useState({
-            color: 'black',
-            backgroundColor: 'white'
-        }
-    )
+  // const [myStyle, setMyStyle] = useState({
+  //           color: 'black',
+  //           backgroundColor: 'white'
+  //       }
+  //   )
 
   const changeMode = () => {
     if(mode === 'light'){
@@ -25,10 +25,10 @@ function App() {
       document.body.style.color = '#fff';
       document.title = 'TextTools - Dark Mode';
       myAlert('Switched to dark mode', 'success')
-      setMyStyle({
-          color: 'white',
-          backgroundColor: 'rgb(40, 40, 40)'
-      })
+      // setMyStyle({
+      //     color: 'white',
+      //     backgroundColor: 'rgb(40, 40, 40)'
+      // })
       // console.log("mode switched to dark")
     }
     else{
@@ -37,10 +37,10 @@ function App() {
       document.body.style.color = '#212529';
       document.title = 'TextTools - light Mode';
       myAlert('Switched to light mode', 'success')
-      setMyStyle({
-          color: 'rgb(40, 40, 40)',
-          backgroundColor: 'white'
-      })
+      // setMyStyle({
+      //     color: 'rgb(40, 40, 40)',
+      //     backgroundColor: 'white'
+      // })
       // console.log("mode switched to light")
     }
   }
@@ -56,27 +56,32 @@ function App() {
     }, 1500);
   }
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <>
-      <Navbar title='TextTools' mode={mode} changeMode={changeMode}/>
-      <Alert alert={alert}/>
-      <TextBox heading='Enter text to make changes' mode={mode} myAlert={myAlert} />
-    </> 
-    },
-    {
-      path: "about",
-      element: <>
-        <Navbar title='TextTools' mode={mode} changeMode={changeMode}/>
-        <Alert alert={alert}/>
-        <About myStyle={myStyle} />
-      </> 
-    },
-  ]);
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <>
+  //     <Navbar title='TextTools' mode={mode} changeMode={changeMode}/>
+  //     <Alert alert={alert}/>
+  //     <TextBox heading='Enter text to make changes' mode={mode} myAlert={myAlert} />
+  //   </> 
+  //   },
+  //   {
+  //     path: "about",
+  //     element: <>
+  //       <Navbar title='TextTools' mode={mode} changeMode={changeMode}/>
+  //       <Alert alert={alert}/>
+  //       <About myStyle={myStyle} />
+  //     </> 
+  //   },
+  // ]);
 
   return (
-    <RouterProvider router={router} />
+    <>
+    <Navbar title='TextTools' mode={mode} changeMode={changeMode}/>
+      <Alert alert={alert}/>
+      <TextBox heading='Enter text to make changes' mode={mode} myAlert={myAlert} />
+    </>
+    // <RouterProvider router={router} />
   );
 }
 
